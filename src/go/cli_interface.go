c5f0c9d970902a70c784c13219b85b32bbe734c6
jameshouston <jjhouston00@outlook.com>
2023-10-13 00:10:41 -0400

    got the text box in as well, need to style it

--- src/go/cli_interface.go
+++ src/go/cli_interface.go
@@ -3,41 +3,45 @@ package main
 import (
 	"fmt"
 	"os"
-	"time"
 
 	"github.com/charmbracelet/bubbles/progress"
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
 type model struct {
-	progress progress.Model
-	percent  float64
+	progressBar progress.Model
+	percent     float64
+	textInput   textinput.Model
 }
 
-func (m *model) Init() tea.Cmd {
+func (m model) Init() tea.Cmd {
 	return nil
 }
 
 func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "q":
 			return m, tea.Quit
 		}
-	case string:
-		if msg == "tick" {
-			m.percent += 2 // Now you can modify it
-			if m.percent >= 100 {
-				return m, tea.Quit
-			}
-		}
 	}
-	return m, tea.Tick(time.Millisecond*100, func(t time.Time) tea.Msg { return "tick" })
+
+	// Update the text input
+	m.textInput, cmd = m.textInput.Update(msg)
+
+	return m, cmd
 }
 
 func (m *model) View() string {
+	// Style the text input
+	inputView := m.textInput.View()
+
+	// Style the box
 	boxStyle := lipgloss.NewStyle().
 		Padding(1).
 		Border(lipgloss.RoundedBorder()).
@@ -47,13 +51,14 @@ func (m *model) View() string {
 	catStyle := lipgloss.NewStyle().
 		Foreground(lipgloss.Color("226"))
 
-	renderedProgress := m.progress.View()
+	renderedProgress := m.progressBar.View()
 
 	ui := []string{
 		boxStyle.Render(
-			fmt.Sprintf("⚡ %s 🐱\n%s",
-				catStyle.Render("Assistant: Fill in your text prompt here"),
+			fmt.Sprintf("⚡ %s 🐱\n%s\n%s",
+				catStyle.Render("Sox: Hey there! What can I do for you?"),
 				renderedProgress,
+				inputView,
 			),
 		),
 	}
@@ -63,7 +68,17 @@ func (m *model) View() string {
 
 func main() {
 	p := progress.NewModel(progress.WithDefaultGradient())
-	m := model{progress: p}
+	input := textinput.NewModel()
+	input.Placeholder = "Type here..."
+	input.Focus()
+	input.CharLimit = 156
+	input.Width = 20
+
+	m := model{
+		progressBar: p,
+		textInput:   input,
+	}
+
 	program := tea.NewProgram(&m)
 	if err := program.Start(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error starting app:\n%s", err)
